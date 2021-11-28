7299e133ec62ae9a9454be3162522311b554cc8e
Winter <33466216+Vortetty@users.noreply.github.com>
2021-11-27 18:39:45 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import argparse
 import os
+import subprocess
 
 parser = argparse.ArgumentParser(description='Converts Brainfuck to C++, then compiles')
 parser.add_argument('input', help='Input file')
-parser.add_argument('-o', '--output', help='Output executable', default='out' + ('.exe' if os.name == 'nt' else ''))
+parser.add_argument('-o', '--output', help='Output executable', default=None, type=str)
 parser.add_argument('-c', '--compiler', help='Compile to use', default='clang++')
 parser.add_argument('-m', '--memory', help='Memory cells needed', default=512, type=int)
+parser.add_argument('-r', '--no-remove-cpp', help='Do not remove C++ file', action='store_false')
 args = parser.parse_args()
+
+if args.output is None:
+    args.output = os.path.splitext(args.input)[0] + ('.exe' if os.name == 'nt' else '')
 
 out_cpp = f"""#include <stdio.h>
 #include <iostream>
@@ -23,11 +28,9 @@ conversion = {
     "+": "    (*i)++;\n",
     "-": "    (*i)--;\n",
     ".": "    std::cout << *i;\n",
-    ",": "    (*i) = (unsigned char)std::getchar();\n",
+    ",": "    std::cout << \"\\nInput requested >>> \";std::cin >> (*i);\n",
     "[": "    while ((*i) != 0) {{\n",
     "]": "    }}\n"
-    #"[": "label_{}:\n    if ((*i) != 0) {{\n",
-    #"]": "    }} if ((*i) != 0)\n    goto label_{};\n"
 }
 
 infile = open(args.input, 'r')
@@ -52,3 +55,8 @@ outfile.write(out_cpp)
 
 infile.close()
 outfile.close()
+
+subprocess.run(f"{args.compiler} {args.output}.cpp -o {args.output}", shell=True)
+
+if args.no_remove_cpp:
+    os.remove(args.output + '.cpp')
